c20bf4167ee8c8fac6c3137b68575faf1c0364f8
ha-rsh20 <88107325+ha-rsh20@users.noreply.github.com>
2022-09-14 23:27:39 +0530

    Delete src directory

--- src/main/java/com/example/stdmgmtsysbacktest/repository/ResultRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.stdmgmtsysbacktest.repository;
-
-import com.example.stdmgmtsysbacktest.model.Result;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResultRepository extends JpaRepository<Result,Integer> {
-}
